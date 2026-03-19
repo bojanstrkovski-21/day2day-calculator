8a92da6d086978e062bcb96be00411854cc178fa
bojanstrkovski-21 <115637403+bojanstrkovski-21@users.noreply.github.com>
2026-03-19 14:38:06 +0100

    Initial commit — day2day calculator with MK holidays

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://bojanstrkovski-21.github.io',
+  base: '/day2day-calculator',
+});
